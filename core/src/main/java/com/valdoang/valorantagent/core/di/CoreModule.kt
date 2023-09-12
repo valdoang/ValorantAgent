0a88aa9a8ba1d2d1b8ecfbb0e3e83e9f07a73a1e
valdoang <valdo.20010@mhs.unesa.ac.id>
2023-09-12 17:31:15 +0800

    Security dan Performance

--- core/src/main/java/com/valdoang/valorantagent/core/di/CoreModule.kt
+++ core/src/main/java/com/valdoang/valorantagent/core/di/CoreModule.kt
@@ -9,6 +9,9 @@ import com.valdoang.valorantagent.core.data.source.remote.RemoteDataSource
 import com.valdoang.valorantagent.core.data.source.remote.network.ApiService
 import com.valdoang.valorantagent.core.domain.repository.IValorantRepository
 import com.valdoang.valorantagent.core.utils.AppExecutors
+import net.sqlcipher.database.SQLiteDatabase
+import net.sqlcipher.database.SupportFactory
+import okhttp3.CertificatePinner
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import org.koin.android.ext.koin.androidContext
@@ -20,19 +23,30 @@ import java.util.concurrent.TimeUnit
 val databaseModule = module {
     factory { get<ValorantDatabase>().valorantDao() }
     single {
+        val passphrase: ByteArray = SQLiteDatabase.getBytes("valdoang".toCharArray())
+        val factory = SupportFactory(passphrase)
         Room.databaseBuilder(
             androidContext(),
             ValorantDatabase::class.java, "Valorant.db"
-        ).fallbackToDestructiveMigration().build()
+        ).fallbackToDestructiveMigration()
+            .openHelperFactory(factory)
+            .build()
     }
 }
 
 val networkModule = module {
     single {
+        val hostname = "valorant-api.com"
+        val certificatePinner = CertificatePinner.Builder()
+            .add(hostname, "sha256/qrCJ+v05fGIiGr+ojQXfXCCVi8Qb0o1qPrxOt0Sd/HY=")
+            .add(hostname, "sha256/81Wf12bcLlFHQAfJluxnzZ6Frg+oJ9PWY/Wrwur8viQ=")
+            .add(hostname, "sha256/hxqRlPTu1bMS/0DITB1SSu0vd4u/8l8TjPgfaAp63Gc=")
+            .build()
         OkHttpClient.Builder()
             .addInterceptor(if(BuildConfig.DEBUG) { HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY) }else { HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.NONE) })
             .connectTimeout(120, TimeUnit.SECONDS)
             .readTimeout(120, TimeUnit.SECONDS)
+            .certificatePinner(certificatePinner)
             .build()
     }
     single {
